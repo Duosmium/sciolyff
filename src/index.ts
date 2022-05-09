d5c79256c4e8ca78087b65364827579a3c1fb12c
Tomi Chen <tctree333@users.noreply.github.com>
2022-05-08 20:02:41 -0700

    add csv converter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import valid, { format } from "./validator/index.js";
 import Interpreter from "./interpreter/index.js";
+import convert from "./convert/index.js";
 
-export default { valid, format, Interpreter };
+export default { valid, format, Interpreter, convert };
---
+++ tests/convert/test.ts
@@ -0,0 +1,30 @@
+import test from "ava";
+
+import fsSync from "fs";
+import fs from "fs/promises";
+import { basename } from "path";
+
+import { csvToSciolyFF } from "../../src/convert/index.js";
+import { load } from "js-yaml";
+
+const casesDir = "tests/convert/cases/";
+
+const testCases = fsSync
+  .readdirSync(casesDir, "utf-8")
+  .map((f) => casesDir + f)
+  .filter((f) => f.endsWith(".csv"));
+
+for (const testCase of testCases) {
+  const name = basename(testCase, ".csv");
+  test("convert: " + name, async (t) => {
+    const csvData = await fs.readFile(testCase, "utf8");
+
+    const expected = load(
+      await fs.readFile(testCase.replace(".csv", ".yaml"), "utf8")
+    );
+
+    const actual = load(csvToSciolyFF(csvData));
+
+    t.deepEqual(actual, expected);
+  });
+}
