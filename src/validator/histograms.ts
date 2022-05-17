4dfb633ccc7b7237df2b118dcaa09e56f702bac3
Tomi Chen <32943907+tctree333@users.noreply.github.com>
2022-05-17 13:03:58 -0700

    update validation

--- src/validator/histograms.ts
+++ src/validator/histograms.ts
@@ -3,11 +3,27 @@ import * as yup from "yup";
 import { root } from "./helpers.js";
 
 const dataSchema = yup.object().shape({
-    event: yup.string(),
+  event: yup
+    .string()
+    .test(
+      "matching-event",
+      "'event: ${value}' in Histograms does not match any event in Events",
+      (value, context) =>
+        root(context)["Events"].some((event) => event.name === value)
+    )
+    .test(
+      "unique-event",
+      "duplicate placing",
+      (value, context) =>
+        root(context)["Histograms"].filter(
+          (histo) => histo.event === value
+        ).length === 1
+    )
+    .required(),
 
-    start: yup.number(),
-    width: yup.number(),
-    count: yup.number()
+  start: yup.number().required(),
+  width: yup.number().min(0).required(),
+  counts: yup.array().of(yup.number().min(0).required()).required()
 })
 
 export default yup.object().shape({
