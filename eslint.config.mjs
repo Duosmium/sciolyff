2395400d5364da8f8f3b3a19819b1a8b8c2df00d
Tomi Chen <tctree333@users.noreply.github.com>
2026-08-11 14:41:45 -0700

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,23 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tseslint from "typescript-eslint";
 import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
-});
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
 	globalIgnores([
 		"**/node_modules/",
 		"**/dist/",
-		"**/eslint.config.mjs",
 		"**/compare/",
 		"**/.*",
 		"**/tests/",
 	]),
 	{
-		extends: compat.extends(
-			"eslint:recommended",
-			"plugin:@typescript-eslint/recommended",
-			"plugin:@typescript-eslint/recommended-requiring-type-checking",
-			"prettier",
-		),
-
-		plugins: {
-			"@typescript-eslint": typescriptEslint,
-		},
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.strictTypeChecked,
+			// tseslint.configs.stylisticTypeChecked,
+		],
 
 		languageOptions: {
 			globals: {
@@ -42,13 +25,8 @@ export default defineConfig([
 				...globals.node,
 			},
 
-			parser: tsParser,
-			ecmaVersion: 5,
-			sourceType: "commonjs",
-
 			parserOptions: {
-				tsconfigRootDir: "./",
-				project: ["./tsconfig.json"],
+				projectService: true,
 			},
 		},
 
@@ -59,6 +37,21 @@ export default defineConfig([
 					"ts-ignore": "allow-with-description",
 				},
 			],
+			"@typescript-eslint/no-invalid-void-type": [
+				"error",
+				{ allowAsThisParameter: true },
+			],
+			"@typescript-eslint/restrict-template-expressions": [
+				"error",
+				{
+					allowNumber: true,
+					allowBoolean: true,
+				},
+			],
+		},
+
+		linterOptions: {
+			reportUnusedInlineConfigs: "error",
 		},
 	},
 	{
@@ -74,4 +67,9 @@ export default defineConfig([
 			"@typescript-eslint/restrict-template-expressions": "off",
 		},
 	},
+	{
+		files: ["eslint.config.mjs"],
+		extends: [tseslint.configs.disableTypeChecked],
+	},
+	eslintConfigPrettier,
 ]);
